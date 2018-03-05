755ce90f23cbf24d07ec73b34550e3cc76f3b461
maxdenstore <31626964+maxdenstore@users.noreply.github.com>
2018-03-05 09:42:54 +0100

    .

--- DAB2/main/Program.cs
+++ DAB2/main/Program.cs
@@ -8,10 +8,10 @@ namespace main
         {
             var personItem = new Item();
 
-            var tlf1 = new Telephone("12345678", "penisCompany", "bootyCalls");
-            var mail1 = new Email("penis@penis.penis");
-            var adr1 = new Adress("penis", "1337a", "penis", "penisBy");
-            var person1 = new Person(tlf1, adr1, "pik", "tissekone", "pek", "multiGender", mail1);
+            var tlf1 = new Telephone("88888888", "TeleDanmark", "Erhverv");
+            var mail1 = new Email("one@two-three.dk");
+            var adr1 = new Adress("Ringvej", "7a", "8000", "Aarhus C");
+            var person1 = new Person(tlf1, adr1, "Jesper", "Jespersen", "Jes", "Mand", mail1);
             personItem.Type = "primary";
             personItem.Adresse = adr1;
             personItem.Personer = person1;
