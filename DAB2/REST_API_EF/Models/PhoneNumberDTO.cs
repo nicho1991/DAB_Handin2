af203901ce959b1f2236c017c641eeffc30b9c54
Max <maxbj@live.dk>
2018-04-28 12:11:43 +0200

    .

--- DAB2/REST_API_EF/Models/PhoneNumberDTO.cs
+++ DAB2/REST_API_EF/Models/PhoneNumberDTO.cs
@@ -7,6 +7,35 @@ namespace REST_API_EF.Models
 {
     public class PhoneNumberDTO
     {
-        public string PhoneNumber { get; set; }
+
+        public int PersonID { get; set; }
+        public string Email { get; set; }
+        public string FamilyName { get; set; }
+        public string Type { get; set; }
+        public string GivenName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phonecompany { get; set; }
+        public string Phonenumber { get; set; }
+        public string Usage { get; set; }
+        public int telephoneId { get; set; }
+
+        public PhoneNumberDTO(Telephone T)
+        {
+            PersonID = T.PersonRefId;
+            Email = T.Person.Email.UniqueEmail;
+            FamilyName = T.Person.FamilyName;
+            Type = T.Person.PersonType;
+            GivenName = T.Person.GivenName;
+            MiddleName = T.Person.GivenName;
+            Phonenumber = T.TelephoneNumber;
+            Phonecompany = T.PhoneCompany;
+            Usage = T.UsageOfTlf;
+            telephoneId = T.telephoneID;
+
+
+        }
+
+
+    }
     }
 }
