2f80d2e796f625872daaeb59e0c06dd62a0e0c01
nicho1991 <ladefoged1991@gmail.com>
2018-04-28 15:18:05 +0200

    ref

--- DAB2/REST_API_DOCDB/Controllers/PeopleController.cs
+++ DAB2/REST_API_DOCDB/Controllers/PeopleController.cs
@@ -1,129 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using DAB2._2;
 using DAB2._2.UnitOfWork;
 using HandIn3._2.Models;
-
+using REST_API_EF.Models;
 
 namespace HandIn3._2.Controllers
 {
     public class PeopleController : ApiController
     {
-        private EFUnitOfWork db = new EFUnitOfWork();
 
-        // GET: api/People
-        public IEnumerable<PersonDTO> GetPeople()
-        {
-            List<PersonDTO> people = new List<PersonDTO>();
+        private DocumentDBUnitOfWork db = new DocumentDBUnitOfWork(new DBContext());
 
-            foreach (Person p in db.Persons)
-            {
-                people.Add(new PersonDTO(p));
-            }
-            return people;
-            //return people.AsQueryable();
-        }
+        //// GET: api/People
+        //public IEnumerable<PersonDTO> GetPeople()
+        //{
+        //    List<PersonDTO> people = new List<PersonDTO>();
 
-        // GET: api/People/5
-        [ResponseType(typeof(PersonDTO))]
-        public async Task<IHttpActionResult> GetPerson(int id)
-        {
-            Person person = await db.Persons.FindAsync(id);
+        //    foreach (Person p in db.Persons)
+        //    {
+        //        people.Add(new PersonDTO(p));
+        //    }
+        //    return people;
+        //    //return people.AsQueryable();
+        //}
 
-            if (person == null)
-            {
-                return NotFound();
-            }
+        //// GET: api/People/5
+        //[ResponseType(typeof(PersonDTO))]
+        //public async Task<IHttpActionResult> GetPerson(int id)
+        //{
+        //    Person person = await db.Persons.FindAsync(id);
 
-            return Ok(new PersonDTO(person));
-        }
+        //    if (person == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-        // PUT: api/People/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutPerson(int id, Person person)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //    return Ok(new PersonDTO(person));
+        //}
 
-            if (id != person.PersonID)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/People/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> PutPerson(int id, Person person)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.Entry(person).State = EntityState.Modified;
+        //    if (id != person.PersonID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PersonExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    db.Entry(person).State = EntityState.Modified;
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!PersonExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-        // POST: api/People
-        [ResponseType(typeof(Person))]
-        public async Task<IHttpActionResult> PostPerson(Person person)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
-            db.Persons.Add(person);
-            await db.SaveChangesAsync();
+        //// POST: api/People
+        //[ResponseType(typeof(Person))]
+        //public async Task<IHttpActionResult> PostPerson(Person person)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            return CreatedAtRoute("DefaultApi", new { id = person.PersonID }, person);
-        }
+        //    db.Persons.Add(person);
+        //    await db.SaveChangesAsync();
 
-        // DELETE: api/People/5
-        [ResponseType(typeof(Person))]
-        public async Task<IHttpActionResult> DeletePerson(int id)
-        {
-            Person person = await db.Persons.FindAsync(id);
-            if (person == null)
-            {
-                return NotFound();
-            }
+        //    return CreatedAtRoute("DefaultApi", new { id = person.PersonID }, person);
+        //}
 
-            db.Persons.Remove(person);
-            await db.SaveChangesAsync();
+        //// DELETE: api/People/5
+        //[ResponseType(typeof(Person))]
+        //public async Task<IHttpActionResult> DeletePerson(int id)
+        //{
+        //    Person person = await db.Persons.FindAsync(id);
+        //    if (person == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(person);
-        }
+        //    db.Persons.Remove(person);
+        //    await db.SaveChangesAsync();
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //    return Ok(person);
+        //}
 
-        private bool PersonExists(int id)
-        {
-            return db.Persons.Count(e => e.PersonID == id) > 0;
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        //private bool PersonExists(int id)
+        //{
+        //    return db.Persons.Count(e => e.PersonID == id) > 0;
+        //}
     }
 }
