1c121bb52f5479bf9cdac3c936d4292e402f0a2c
Max <maxbj@live.dk>
2018-04-28 11:53:37 +0200

    .

---
+++ DAB2/REST_API_EF/Models/SecondaryAdress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API_EF.Models
+{
+    public class SecondaryAdressDTO
+    {
+        public int Id { get; set; }
+
+        public Adress SecondaryAdressAdress { get; set; }
+
+        public SecondaryAdressDTO(Adress adr)
+        {
+            SecondaryAdressAdress = adr;
+        }
+    }
+}
